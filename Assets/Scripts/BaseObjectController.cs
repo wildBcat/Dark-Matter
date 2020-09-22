2bba08cf75b1f387b6f07f2df1367945edf5fa23
wildBcat <sundance.eaton@icloud.com>
2020-09-21 19:01:19 -0600

    Cleaned up the BaseObjectController code.

--- Assets/Scripts/BaseObjectController.cs
+++ Assets/Scripts/BaseObjectController.cs
@@ -4,52 +4,60 @@ using UnityEngine;
 
 public class BaseObjectController : MonoBehaviour
 {
+    [Tooltip("Sets the amount of damage game object deals on collision with another object. " +
+        "The higher the number, the more damage it assigns during each collision.")]
     [SerializeField] int damage = default;
 
+    [Tooltip("Sets The amount of health game object has. Any damage assigned to the game object will reduce this number. " +
+        "The higher the number, the more it will take to kill the game object.")]
     [SerializeField] int health = default;
 
+    [Tooltip("Sets the color the sprite will flash when damage is assigned. " +
+        "If this isn't set then the mat will take on the color of the sprite.")]
     [SerializeField] Color collideColor = default;
 
-    Color normalColor;
-
-    private Material matWhite;
-    private Material matDefault;
-    SpriteRenderer sr;
-
-
+    private Material matWhite;    
+    private Material matDefault;    
     SpriteRenderer[] renderers;
     Color[] originalColors;
+
     private void Start()
     {
+        // Loads the array with each child sprite's sprite renderer
         renderers = GetComponentsInChildren<SpriteRenderer>();
+
+        // Sets the number of original colors to load
         originalColors = new Color[renderers.Length];
 
+        // Loads the array with each child sprite's sprite renderer color
         for (int i = 0; i < originalColors.Length; i++)
         {
             originalColors[i] = renderers[i].color;
         }
 
-        sr = GetComponent<SpriteRenderer>();
-
+        // Loads the mask material for each child sprite and matches the matDefault with the original sprite renderer material
         foreach (SpriteRenderer sr in GetComponentsInChildren<SpriteRenderer>())
         {
             matWhite = Resources.Load("WhiteFlash", typeof(Material)) as Material;
             matDefault = sr.GetComponent<SpriteRenderer>().material;
         }
     }
-
+    
     public int GetDamage()
     {
+        // Assigns damage
         return damage;
     }
-
+    
     public void ProjectileHit()
     {
+        // Destroys projectile on impact
         Destroy(gameObject);
     }
 
     private void OnTriggerEnter2D(Collider2D collidingGameObject)
     {
+        // Checks to see if the colliding object uses the BaseObjectController script. If it does, it processes a hit
         BaseObjectController baseObjectController = collidingGameObject.gameObject.GetComponent<BaseObjectController>();
         if (!baseObjectController)
         {
@@ -60,44 +68,63 @@ public class BaseObjectController : MonoBehaviour
 
     private void ProcessHit(BaseObjectController baseObjectController)
     {
-        StartCoroutine(Flash());
-        
+        // Decreased the health of the game object by the collinding object's damage amount
         health -= baseObjectController.GetDamage();
+
+        // If the received damage reduces the game objects health to 0 or less it destroys it, otherwise it initiates a damage flash
         if (health <= 0)
         {
             Die();
         }
-    }
-
-    private void Die()
-    {
-        ExplosionController[] allExplosions = GameObject.FindObjectsOfType<ExplosionController>();
-        foreach (ExplosionController oneExpl in allExplosions)
-            oneExpl.StartExplosion();
+        else
+        {
+            if(!CompareTag("Asteroid"))
+            {
+                StartCoroutine(Flash());
+            }
+        }
     }
 
     IEnumerator Flash()
     {
-        for (int j = 0; j < 5; j++)
+        for (int j = 0; j < 3; j++)
         {
             foreach (SpriteRenderer r in renderers)
             {
+                // Sets each sprite in the renderers array to the color set in the collideColor variable
                 r.color = collideColor;
             }
             foreach (SpriteRenderer sr in GetComponentsInChildren<SpriteRenderer>())
             {
+                // Sets the material in each child sprite's sprite renderer to white
                 sr.material = matWhite;
             }
+
             yield return new WaitForSeconds(.1f);
+            
             for (int i = 0; i < originalColors.Length; i++)
             {
+                // Returns each sprite in the renderers array to the color set in the originalColors variable
                 renderers[i].color = originalColors[i];
             }
             foreach (SpriteRenderer sr in GetComponentsInChildren<SpriteRenderer>())
             {
+                // Returns all the child sprite's sprite renderer material back to what it was
                 sr.material = matDefault;
             }
             yield return new WaitForSeconds(.1f);
         }
     }
+
+    private void Die()
+    {
+        // Assigns the allExplosions variable to all the objects with an expolosion controller script
+        ExplosionController[] allExplosions = GameObject.FindObjectsOfType<ExplosionController>();
+
+        // Triggers the explosion for each one stored in allExplosions
+        foreach (ExplosionController oneExpl in allExplosions)
+            oneExpl.StartExplosion();
+    }
+
+
 }
---
+++ Library/Collab/Download/Assets/Scripts/BaseObjectController.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseObjectController : MonoBehaviour
+{
+    [Tooltip("Sets the amount of damage game object deals on collision with another object. The higher the number, the more damage it assigns during each collision.")]
+    [SerializeField] int damage = default;
+
+    [Tooltip("Sets The amount of health game object has. Any damage assigned to the game object will reduce this number. The higher the number, the more it will take to kill the game object.")]
+    [SerializeField] int health = default;
+
+    [Tooltip("Sets the color the game object will flash when damage is assigned.")]
+    [SerializeField] Color collideColor = default;
+
+    private Material matWhite;
+    private Material matDefault;
+    SpriteRenderer sr;
+    SpriteRenderer[] renderers;
+    Color[] originalColors;
+
+    private void Start()
+    {
+        renderers = GetComponentsInChildren<SpriteRenderer>();
+        originalColors = new Color[renderers.Length];
+
+        for (int i = 0; i < originalColors.Length; i++)
+        {
+            originalColors[i] = renderers[i].color;
+        }
+
+        sr = GetComponent<SpriteRenderer>();
+
+        foreach (SpriteRenderer sr in GetComponentsInChildren<SpriteRenderer>())
+        {
+            matWhite = Resources.Load("WhiteFlash", typeof(Material)) as Material;
+            matDefault = sr.GetComponent<SpriteRenderer>().material;
+        }
+    }
+
+    public int GetDamage()
+    {
+        return damage;
+    }
+
+    public void ProjectileHit()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collidingGameObject)
+    {
+        BaseObjectController baseObjectController = collidingGameObject.gameObject.GetComponent<BaseObjectController>();
+        if (!baseObjectController)
+        {
+            return;
+        }
+        ProcessHit(baseObjectController);
+    }
+
+    private void ProcessHit(BaseObjectController baseObjectController)
+    {       
+        health -= baseObjectController.GetDamage();
+        if (health <= 0)
+        {
+            Die();
+        }
+        else
+        {
+            if(!CompareTag("Asteroid"))
+            {
+                StartCoroutine(Flash());
+            }
+        }
+    }
+
+    IEnumerator Flash()
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            foreach (SpriteRenderer r in renderers)
+            {
+                r.color = collideColor;
+            }
+            foreach (SpriteRenderer sr in GetComponentsInChildren<SpriteRenderer>())
+            {
+                sr.material = matWhite;
+            }
+            yield return new WaitForSeconds(.1f);
+            for (int i = 0; i < originalColors.Length; i++)
+            {
+                renderers[i].color = originalColors[i];
+            }
+            foreach (SpriteRenderer sr in GetComponentsInChildren<SpriteRenderer>())
+            {
+                sr.material = matDefault;
+            }
+            yield return new WaitForSeconds(.1f);
+        }
+    }
+
+    private void Die()
+    {
+        ExplosionController[] allExplosions = GameObject.FindObjectsOfType<ExplosionController>();
+        foreach (ExplosionController oneExpl in allExplosions)
+            oneExpl.StartExplosion();
+    }
+
+
+}
