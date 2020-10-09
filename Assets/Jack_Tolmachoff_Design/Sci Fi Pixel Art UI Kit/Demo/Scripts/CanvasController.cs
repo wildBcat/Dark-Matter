72afc4b126f1d5d29c76ed5b248791f1d81edb1b
wildBcat <sundance.eaton@icloud.com>
2020-10-09 16:50:06 -0600

    More changes than there should've been.
    I can't even remember them all. I started with making a new player selection scene that is more to my liking. Got side trackied and began working on making my game more pixel perfect. Then I installed a hierarchy package to get rid of folders in the hierarchy. Along the way I made a lot of changes that most likely broke things.

---
+++ Assets/Jack_Tolmachoff_Design/Sci Fi Pixel Art UI Kit/Demo/Scripts/CanvasController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasController : MonoBehaviour
+{
+    #region Public Variables
+    public bool isTimedScreen = false;
+    public float screenTime = 3f;
+    public GameObject targetTimedScreen;
+    #endregion
+
+    #region Private Variables
+    private float startTime = 0f;
+    #endregion
+
+    #region Main Methods
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetupCanvas();
+    }
+
+    private void OnEnable()
+    {
+        SetupCanvas();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (isTimedScreen)
+        {
+            while (Time.time < startTime + screenTime)
+            {
+                return;
+            }
+            if (targetTimedScreen)
+            {
+                gameObject.SetActive(false);
+                targetTimedScreen.SetActive(true);
+            }
+        }
+    }
+    #endregion
+    #region Utility Methods
+    void SetupCanvas()
+    {
+        startTime = Time.time;
+    }
+    #endregion
+}
